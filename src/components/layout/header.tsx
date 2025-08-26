ad11ce03513f917f40b196c2d3403f00d5da9330
Nika Abashidze <n.abashidze25@gmail.com>
2025-08-26 21:14:59 +0000

    change the color of the panel to grey or something

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -158,7 +158,7 @@ export function Header() {
   }
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-card/95 backdrop-blur supports-[backdrop-filter]:bg-card/60">
       <div className="container flex h-16 items-center">
         <div className="mr-auto flex items-center">
            <Link 
