3d333c260ed3308c507d3e9e949c9337bbdab8a7
Nika Abashidze <n.abashidze25@gmail.com>
2025-08-26 21:33:40 +0000

    I added the logo-midnight.png in the public folder. you can use it for t

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -14,7 +14,7 @@ export function TesteryLogo({ className }: { className?: string }) {
                 data-ai-hint="logo light" 
             />
             <Image 
-                src="/logo-dark.png" 
+                src="/logo-midnight.png" 
                 alt="Testery Logo" 
                 width={150} 
                 height={40}
